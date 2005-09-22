449e4cc81b084d2799bdb6424a77397e92f6cfaa
J. Peter Mugaas <J. Peter Mugaas@421b718a-868e-4740-9b41-4026df17543c>
2005-09-22 22:25:00 +0000

    (empty log message)

---
+++ branches/FreePascal Port/Indy10/Lib/Protocols/Res/makelrz.bat
@@ -0,0 +1,2 @@
+c:\lazarus\tools\lazres IdSASLListEditorForm.lrs ARROWDOWN.xpm ARROWLEFT.xpm ARROWRIGHT.xpm ARROWUP.xpm DIS_ARROWDOWN.xpm DIS_ARROWLEFT.xpm DIS_ARROWRIGHT.xpm DIS_ARROWUP.xpm
+copy *.lrs ..
