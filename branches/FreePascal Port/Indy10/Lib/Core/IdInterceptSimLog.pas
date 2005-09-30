418c4f58f1bb7514e92003c71c5966468821345d
J. Peter Mugaas <J. Peter Mugaas@421b718a-868e-4740-9b41-4026df17543c>
2005-09-30 19:36:35 +0000

    Case.

--- branches/FreePascal Port/Indy10/Lib/Core/IdInterceptSimLog.pas
+++ branches/FreePascal Port/Indy10/Lib/Core/IdInterceptSimLog.pas
@@ -54,7 +54,7 @@ TODO: Can also change it to detect several EOLs and non binary and use :Lines:x
 }
 
 interface
-{$i idcompilerdefines.inc}
+{$i IdCompilerDefines.inc}
 
 uses
   IdGlobal, IdIntercept, IdBaseComponent, IdObjs;
