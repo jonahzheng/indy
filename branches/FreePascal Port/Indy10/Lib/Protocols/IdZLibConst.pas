53a5df5246c82f58ff0b561fbcec3f5346c2dc39
J. Peter Mugaas <J. Peter Mugaas@421b718a-868e-4740-9b41-4026df17543c>
2006-02-25 17:17:54 +0000

    (empty log message)

---
+++ branches/FreePascal Port/Indy10/Lib/Protocols/IdZLibConst.pas
@@ -0,0 +1,21 @@
+
+{*******************************************************}
+{                                                       }
+{       Borland Delphi Visual Component Library         }
+{                                                       }
+{  Copyright (c) 1997-1999 Borland Software Corporation }
+{                                                       }
+{*******************************************************}
+
+unit IdZLibConst;
+
+interface
+{$i IdCompilerDefines.inc}
+resourcestring
+ sTargetBufferTooSmall = 'ZLib error: target buffer may be too small';
+ sInvalidStreamOp = 'Invalid stream operation';
+
+implementation
+
+end.
+
