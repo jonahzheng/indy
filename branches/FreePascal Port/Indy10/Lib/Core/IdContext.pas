cff5e6a86a3b3f1c505607a7a274e103949edd9a
J. Peter Mugaas <J. Peter Mugaas@421b718a-868e-4740-9b41-4026df17543c>
2006-03-04 05:18:39 +0000

    Merged fixes from main branch.

--- branches/FreePascal Port/Indy10/Lib/Core/IdContext.pas
+++ branches/FreePascal Port/Indy10/Lib/Core/IdContext.pas
@@ -16,63 +16,52 @@
   $Log$
 }
 {
-   Rev 1.14    6/16/2004 2:08:48 PM  JPMugaas
- Binding made public for the FTP Server.
-}
-{
-    Rev 1.13    6/4/2004 1:34:24 PM  DSiders
+  Rev 1.14    6/16/2004 2:08:48 PM  JPMugaas
+  Binding made public for the FTP Server.
+
+  Rev 1.13    6/4/2004 1:34:24 PM  DSiders
   Removed unused TIdContextDoRun, TIdContextMethod types.
-}
-{
-   Rev 1.12    2004.02.03 4:17:08 PM  czhower
- For unit name changes.
-}
-{
-   Rev 1.11    21.1.2004 ã. 12:31:04  DBondzhev
- Fix for Indy source. Workaround for dccil bug
- now it can be compiled using Compile instead of build
-}
-{
-   Rev 1.10    2003.10.21 12:18:58 AM  czhower
- TIdTask support and fiber bug fixes.
-}
-{
-   Rev 1.9    2003.10.11 5:47:18 PM  czhower
- -VCL fixes for servers
- -Chain suport for servers (Super core)
- -Scheduler upgrades
- -Full yarn support
-}
-{
-   Rev 1.8    2003.09.19 11:54:28 AM  czhower
- -Completed more features necessary for servers
- -Fixed some bugs
-}
-{
-   Rev 1.7    3/22/2003 09:45:26 PM  JPMugaas
- Now should compile under D4.
-}
-{
-    Rev 1.6    3/13/2003 10:18:38 AM  BGooijen
+
+  Rev 1.12    2004.02.03 4:17:08 PM  czhower
+  For unit name changes.
+
+  Rev 1.11    21.1.2004 ã. 12:31:04  DBondzhev
+  Fix for Indy source. Workaround for dccil bug
+  now it can be compiled using Compile instead of build
+
+  Rev 1.10    2003.10.21 12:18:58 AM  czhower
+  TIdTask support and fiber bug fixes.
+
+  Rev 1.9    2003.10.11 5:47:18 PM  czhower
+  -VCL fixes for servers
+  -Chain suport for servers (Super core)
+  -Scheduler upgrades
+  -Full yarn support
+
+  Rev 1.8    2003.09.19 11:54:28 AM  czhower
+  -Completed more features necessary for servers
+  -Fixed some bugs
+
+  Rev 1.7    3/22/2003 09:45:26 PM  JPMugaas
+  Now should compile under D4.
+
+  Rev 1.6    3/13/2003 10:18:38 AM  BGooijen
   Server side fibers, bug fixes
-}
-{
-    Rev 1.5    1/31/2003 7:24:18 PM  BGooijen
+
+  Rev 1.5    1/31/2003 7:24:18 PM  BGooijen
   Added a .Binding function
+
+  Rev 1.4    1/23/2003 8:33:20 PM  BGooijen
+
+  Rev 1.3    1/23/2003 11:06:06 AM  BGooijen
+
+
+  Rev 1.2    1-17-2003 23:58:30  BGooijen
+  removed OnCreate/OnDestroy again, they had no use
+
+  Rev 1.0    1-17-2003 22:28:58  BGooijen
 }
-{
-    Rev 1.4    1/23/2003 8:33:20 PM  BGooijen
-}
-{
-    Rev 1.3    1/23/2003 11:06:06 AM  BGooijen
-}
-{
-   Rev 1.2    1-17-2003 23:58:30  BGooijen
- removed OnCreate/OnDestroy again, they had no use
-}
-{
-   Rev 1.0    1-17-2003 22:28:58  BGooijen
-}
+
 unit IdContext;
 
 interface
@@ -100,9 +89,9 @@ type
     FOnBeforeRun: TIdContextBeforeRun;
     FOnAfterRun: TIdContextAfterRun;
     //
-    procedure AfterRun; override;
     procedure BeforeRun; override;
     function Run: Boolean; override;
+    procedure AfterRun; override;
   public
     constructor Create(
       AConnection: TIdTCPConnection;
@@ -140,16 +129,14 @@ begin
   FConnection := AConnection;
   FOwnsConnection:=True;
   FContextList := AList;
-  if FContextList <> nil then begin
-    FContextList.Add(Self);
-  end;
 end;
 
 destructor TIdContext.Destroy;
 begin
-  if FContextList <> nil then begin
+  if FContextList<>nil then begin
     FContextList.Remove(Self);
   end;
+
   if FOwnsConnection then begin
     Sys.FreeAndNil(FConnection);
   end;
@@ -163,6 +150,13 @@ end;
 
 procedure TIdContext.BeforeRun;
 begin
+  //Context must be added to ContextList outside of create. This avoids
+  //the possibility of another thread accessing a context (specifically
+  //a subclass) that is still creating. similar logic for remove/destroy.
+  if FContextList <> nil then begin
+    FContextList.Add(Self);
+  end;
+
   if Assigned(OnBeforeRun) then begin
     OnBeforeRun(Self);
   end;
@@ -181,6 +175,10 @@ procedure TIdContext.AfterRun;
 begin
   if Assigned(OnAfterRun) then begin
     OnAfterRun(Self);
+  end;
+
+  if FContextList <> nil then begin
+    FContextList.Remove(Self);
   end;
 end;
 
