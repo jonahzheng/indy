8b229c57e8638145692966cb06fcfebb1bb17812
J. Peter Mugaas <J. Peter Mugaas@421b718a-868e-4740-9b41-4026df17543c>
2006-04-04 00:02:16 +0000

    Updated stuff to use a BZipped tarball instead of a gzipped tarball.

--- branches/FreePascal Port/Indy10/Lib/makeindyrpm.sh
+++ branches/FreePascal Port/Indy10/Lib/makeindyrpm.sh
@@ -12,19 +12,20 @@ echo "Extracting Program and RPM Values:"
 # is tied to the EXACT version of the fpc-src RPM you used 
 # you have to prevent errors.
 FPCVER=`fpc -iV`
-FPCRPMVER=`rpm -qa | grep 'fpc-[[:digit:]]'`
+FPCRPMVER=$(rpm -qa | egrep '^fpc-[0-9]')
 FPCRPMVER=${FPCRPMVER:4}
-FPCSRCRPMVER=`rpm -qa | grep 'fpc-src-[[:digit:]]'`
+FPCSRCRPMVER=$(rpm -qa | egrep '^fpc-src-[0-9]')
 FPCSRCRPMVER=${FPCSRCRPMVER:8}
-echo "$FPCVER"
-echo "$FPCRPMVER"
-echo "$FPCSRCRPMVER"
+echo "FPC Version: $FPCVER"
+echo "FPC RPM Ver: $FPCRPMVER"
+echo "FPCSRC RPM Ver: $FPCSRCRPMVER"
+echo "copying tarball to ~/rpmbuild/SOURCES"
+cp indy*.tar.bz2 ~/rpmbuild/SOURCES
+echo "Making RPM spec file"
   cat indy-fpc.spec.template| \
-    sed -e 's/^%define _FPC_Version .*/%define _FPC_Version '"$FPCVER/" \
-        -e 's/^%define _FPC_RPM_Ver .*/%define _FPC_RPM_Ver '"$FPCRPMVER/" \
-        -e 's/^%define _FPC_SRC_RPM_Ver .*/%define _FPC_SRC_RPM_Ver '"$FPCSRCRPMVER/" \
+    sed -e "s/^%define _FPC_Version .*/%define _FPC_Version $FPCVER/" \
+        -e "s/^%define _FPC_RPM_Ver .*/%define _FPC_RPM_Ver $FPCRPMVER/" \
+        -e "s/^%define _FPC_SRC_RPM_Ver .*/%define _FPC_SRC_RPM_Ver $FPCSRCRPMVER/" \
     > indy-fpc.spec
-rpmbuild -ba indy-fpc.spec
-.spec
-echo "Building indy-fpc.spec"
-rpmbuild -ba indy-fpc.spec > /dev/null
+echo "Building RPM"
+rpmbuild -bb -v indy-fpc.spec
