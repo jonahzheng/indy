d12d87f11755a146d51a94f4df73291db1bae1b1
RemyLebeau <RemyLebeau@421b718a-868e-4740-9b41-4026df17543c>
2009-02-12 11:09:57 +0000

    Bug fixes in TIdHeaderCoderUTF

--- branches/Tiburon/Lib/Protocols/IdHeaderCoderUTF.pas
+++ branches/Tiburon/Lib/Protocols/IdHeaderCoderUTF.pas
@@ -19,56 +19,81 @@ implementation
 
 class function TIdHeaderCoderUTF.Decode(const ACharSet, AData: String): String;
 var
-  LEncoder: TIdTextEncoding;
+  LEncoding: TIdTextEncoding;
   LBytes: TIdBytes;
 begin
   Result := '';
   LBytes := nil;
   if TextIsSame(ACharSet, 'UTF-7') then begin {do not localize}
-    LEncoder := TIdTextEncoding.UTF7;
+    LEncoding := TIdTextEncoding.UTF7;
   end
   else if TextIsSame(ACharSet, 'UTF-8') then begin {do not localize}
-    LEncoder := TIdTextEncoding.UTF8;
+    LEncoding := TIdTextEncoding.UTF8;
   end else
   begin
     Exit;
   end;
+  {$IFDEF DOTNET_OR_UNICODESTRING}
+  // RLebeau 1/27/09: do not use the same Encoding class to decode the input
+  // string to bytes and then decode the bytes to a string.  Doing so will
+  // undo what TIdTextEncoding.Convert() does, effectively making this class
+  // behave the same as TIdHeaderCoderPlain.  The output of this class needs
+  // to be a string that contains codeunits in the UTF-16 range, not
+  // codeunits that have been converted back to the input encoding...
+  LBytes := Get8BitEncoding.GetBytes(AData);
+  {$ELSE}
+  // RLebeau 2/12/09: Not using TIdTextEncoding.GetBytes() here. Although
+  // the input string (should) contain the correct values, the conversion
+  // performed by the RTL when assigning an AnsiString to the WideString
+  // parameter can change characters!!  Just assign the input characters
+  // directly to the buffer to avoid that...
+  if AData <> '' then begin
+    LBytes := RawToBytes(PChar(AData)^, Length(AData));
+  end;
+  {$ENDIF}
   LBytes := TIdTextEncoding.Convert(
-    LEncoder,
+    LEncoding,
     TIdTextEncoding.Unicode,
-    LEncoder.GetBytes(AData));
+    LBytes);
   Result := TIdTextEncoding.Unicode.GetString(LBytes, 0, Length(LBytes));
 end;
 
 class function TIdHeaderCoderUTF.Encode(const ACharSet, AData: String): String;
 var
   LBytes: TIdBytes;
-  LStrEncoding, LByteEncoding: TIdTextEncoding;
+  LEncoding: TIdTextEncoding;
 begin
   Result := '';
   LBytes := nil;
-  // RLebeau 1/27/09: do not use the same Encoding class to encode the input
-  // string to bytes and then encode the bytes to a string.  Doing so will
-  // undo what TIdTextEncoding.Convert() does, effectively making this class
-  // behave the same as TIdHeaderCoderPlain.  The output of this class needs
-  // to be a string that contains codeunits in the UTF-7/8 Ansi range, not
-  // codeunits that have been converted back to UTF-16...
   if TextIsSame(ACharSet, 'UTF-7') then begin {do not localize}
-    LStrEncoding := TIdTextEncoding.UTF7;
-    LByteEncoding := TIdTextEncoding.ASCII;
+    LEncoding := TIdTextEncoding.UTF7;
   end
   else if TextIsSame(ACharSet, 'UTF-8') then begin {do not localize}
-    LStrEncoding := TIdTextEncoding.UTF8;
-    LByteEncoding := Get8BitEncoding;
+    LEncoding := TIdTextEncoding.UTF8;
   end else
   begin
     Exit;
   end;
   LBytes := TIdTextEncoding.Convert(
     TIdTextEncoding.Unicode,
-    LStrEncoding,
+    LEncoding,
     TIdTextEncoding.Unicode.GetBytes(AData));
-  Result := LByteEncoding.GetString(LBytes, 0, Length(LBytes));
+  {$IFDEF DOTNET_OR_UNICODESTRING}
+  // RLebeau 1/27/09: do not use the same Encoding class to encode the input
+  // string to bytes and then encode the bytes to a string.  Doing so will
+  // undo what TIdTextEncoding.Convert() does, effectively making this class
+  // behave the same as TIdHeaderCoderPlain.  The output of this class needs
+  // to be a string that contains codeunits in the UTF-7/8 Ansi range, not
+  // codeunits that have been converted back to UTF-16...
+  Result := Get8BitEncoding.GetString(LBytes, 0, Length(LBytes));
+  {$ELSE}
+  // RLebeau 2/12/09: Not using TIdTextEncoding.GetString() here. Although
+  // the encoded bytes contain the correct values, the conversion performed
+  // by the RTL when assigning a WideString to the AnsiString Result can
+  // lose characters!!  Just assign the encoded bytes directly to the Result
+  // to avoid that...
+  SetString(Result, PAnsiChar(LBytes), Length(LBytes));
+  {$ENDIF}
 end;
 
 class function TIdHeaderCoderUTF.CanHandle(const ACharSet: String): Boolean;
