8e9a0fe468d8d5a3d77c12011138af1bdc4c87b3
JPeterMugaas <JPeterMugaas@421b718a-868e-4740-9b41-4026df17543c>
2009-08-14 12:47:53 +0000

    VCL.NET fix for duplicate identifiers.

--- branches/Tiburon/Lib/Protocols/IdResourceStringsProtocols.pas
+++ branches/Tiburon/Lib/Protocols/IdResourceStringsProtocols.pas
@@ -986,10 +986,12 @@ resourcestring
   // TIdURI
   {$IFDEF STRING_IS_UNICODE}
     {$IFNDEF HAS_TCharacter}
+       {$IFNDEF DOTNET}
   RSUTF16IndexOutOfRange = 'Character Index %d out of Range, Length = %d';
   RSUTF16InvalidHighSurrogate = 'Character at Index %d is not a valid UTF-16 High Surrogate';
   RSUTF16InvalidLowSurrogate = 'Character at Index %d is not a valid UTF-16 Low Surrogate';
   RSUTF16MissingLowSurrogate = 'Missing a Low Surrogate in UTF-16 sequence';
+        {$ENDIF}
     {$ENDIF}
   {$ENDIF}
 
