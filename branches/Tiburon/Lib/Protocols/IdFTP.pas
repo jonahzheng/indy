d7a5e2495014212d97dd4ca1c2c3cf9ba31e6588
Indy-JPeterMugaas <Indy-JPeterMugaas@421b718a-868e-4740-9b41-4026df17543c>
2009-10-31 23:44:06 +0000

    Facilitate inlining in RTL.

--- branches/Tiburon/Lib/Protocols/IdFTP.pas
+++ branches/Tiburon/Lib/Protocols/IdFTP.pas
@@ -1074,6 +1074,11 @@ uses
   {$IFDEF WIN32_OR_WIN64_OR_WINCE}
     {$IFDEF USE_INLINE}
   Windows,
+    {$ELSE}
+  //facilitate inlining only.
+      {$IFDEF VCL_2009_OR_ABOVE}
+  Windows,
+      {$ENDIF}
     {$ENDIF}
   {$ENDIF}
   {$IFDEF DOTNET}
