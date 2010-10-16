68ab2d7dc1e0e557e377eb3bcc30d5803ca90923
Indy-JPeterMugaas <Indy-JPeterMugaas@421b718a-868e-4740-9b41-4026df17543c>
2010-10-16 19:53:18 +0000

    Now works with OSX and it's volume mounting.  This was tested using the FreePascal 2.4.0 OSX compiler .dmg.  There's still more work to do on it.

--- branches/Tiburon/getindy.sh
+++ branches/Tiburon/getindy.sh
@@ -1,5 +1,22 @@
 #!/bin/sh
-INDYVERSION=`cat /mnt/hgfs/IndyTiburon/lib/System/IdVers.inc | grep ' *gsIdVersion *=.*;' | sed -e 's/[^0-9.]//g'` 
+OS=`uname -s`
+if [ "$OS" = 'Darwin' ]
+then
+  S_PREFIX='/Volumes/E$/source/indy10/branches/Tiburon'
+  DOS2_UNIX='tr -d ''\r' 
+  echo $DOS2_UNIX
+  FPCSRC=/Developer/FreePascalCompiler/$(fpc -iV)/Source
+else
+  S_PREFIX='/mnt/hgfs/IndyTiburon'
+  DOS2_UNIX='dos2unix'
+  FPCSRC=/usr/share/fpcsrc/$(fpc -iV)
+  if [ ! -d $FPCSRC ]
+  then
+    FPCSRC=/usr/share/fpcsrc
+  fi
+fi
+
+INDYVERSION=`cat $S_PREFIX/lib/System/IdVers.inc | grep ' *gsIdVersion *=.*;' | sed -e 's/[^0-9.]//g'` 
 INDYDIR=indy-$INDYVERSION
 FPCINDYDIR=$INDYDIR/fpc
 LAZINDYDIR=$INDYDIR/lazarus
@@ -9,33 +26,30 @@ if [ "$1" != "buildonly" ]
 then
   rm -rf $INDYDIR
   mkdir -p $INDYDIR/fpc 
-  cp /mnt/hgfs/IndyTiburon/lib/fpcnotes/* $INDYDIR
-  cp /mnt/hgfs/IndyTiburon/lib/makeindyrpm.sh $INDYDIR
-  cp /mnt/hgfs/IndyTiburon/lib/indy-fpc.spec.template $INDYDIR
-  FILENAMES=$(cat /mnt/hgfs/IndyTiburon/lib/RTFileList.txt | dos2unix | tr '\\' '/') 
+
+  cp $S_PREFIX/lib/fpcnotes/* $INDYDIR
+  cp $S_PREFIX/lib/makeindyrpm.sh $INDYDIR
+  cp $S_PREFIX/lib/indy-fpc.spec.template $INDYDIR
+  FILENAMES=$(cat $S_PREFIX/lib/RTFileList.txt | $DOS2_UNIX | tr '\\' '/') 
   for i in $FILENAMES
   do
-    cp /mnt/hgfs/IndyTiburon/lib/$i $FPCINDYDIR
+    cp $S_PREFIX//lib/$i $FPCINDYDIR
   done
-  cp /mnt/hgfs/IndyTiburon/lib/System/IdCompilerDefines.inc $FPCINDYDIR
-  cp /mnt/hgfs/IndyTiburon/lib/System/IdVers.inc $FPCINDYDIR
-  cp /mnt/hgfs/IndyTiburon/lib/System/indysystemfpc.pas $FPCINDYDIR
-  cp /mnt/hgfs/IndyTiburon/lib/Core/indycorefpc.pas $FPCINDYDIR
-  cp /mnt/hgfs/IndyTiburon/lib/Protocols/indyprotocolsfpc.pas $FPCINDYDIR
-  cp /mnt/hgfs/IndyTiburon/lib/indymaster-Makefile.fpc $FPCINDYDIR/Makefile.fpc
+  cp $S_PREFIX/lib/System/IdCompilerDefines.inc $FPCINDYDIR
+  cp $S_PREFIX/lib/System/IdVers.inc $FPCINDYDIR
+  cp $S_PREFIX/lib/System/indysystemfpc.pas $FPCINDYDIR
+  cp $S_PREFIX/lib/Core/indycorefpc.pas $FPCINDYDIR
+  cp $S_PREFIX/lib/Protocols/indyprotocolsfpc.pas $FPCINDYDIR
+  cp $S_PREFIX/lib/indymaster-Makefile.fpc $FPCINDYDIR/Makefile.fpc
   mkdir $FPCINDYDIR/examples
-  cp -r /mnt/hgfs/IndyTiburon/lib/Examples/* $FPCINDYDIR/examples 
+  cp -r $S_PREFIX/lib/Examples/* $FPCINDYDIR/examples 
   find $FPCINDYDIR/examples -type d -name ".svn" -exec rm -rf '{}' \;
   mkdir $FPCINDYDIR/debian
-  cp -r /mnt/hgfs/IndyTiburon/lib/debian/* $FPCINDYDIR/debian
+  cp -r $S_PREFIX/lib/debian/* $FPCINDYDIR/debian
   find $FPCINDYDIR/debian -type d -name ".svn" -exec rm -rf '{}' \;
 fi
 make
-FPCSRC=/usr/share/fpcsrc/$(fpc -iV)
-if [ ! -d $FPCSRC ]
-then
-  FPCSRC=/usr/share/fpcsrc
-fi
+
 FPCDIR=$FPCSRC;export FPCDIR
 cd $FPCINDYDIR
 echo $(pwd)
